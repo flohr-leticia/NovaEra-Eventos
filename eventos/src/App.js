e3da129e253abf609e369416042f69a6025c3027
LETICIA FLOHR ATANAZIO <4548185932@estudante.cedupguaramirim.com.br>
2026-05-21 10:42:46 -0300

    git inicial

---
+++ eventos/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" className="App-logo"/>
+        <p>
+          
+         Plataforma de Eventos
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saiba Mais
+
+        </a>
+        <p>
+          Transforme momentos em experiências inesquecíveis com a NovaEra Eventos a plataforma que conecta pessoas aos melhores eventos da nova geração.
+        </p>
+        <p>Bem-vindo à NovaEra Eventos, a plataforma criada para transformar a maneira como você descobre, organiza e participa de eventos. Aqui você encontra shows, festas, palestras, workshops e experiências únicas em um só lugar.
+
+Com uma interface moderna e prática, a NovaEra Eventos conecta pessoas, ideias e experiências inesquecíveis. Seja para divulgar um grande evento ou encontrar a próxima experiência marcante da sua vida, nossa plataforma oferece tudo o que você precisa de forma rápida, segura e intuitiva.
+
+Descubra uma nova forma de viver eventos. Viva a Nova Era.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
